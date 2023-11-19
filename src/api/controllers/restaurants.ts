aa44c20a1292e323287bbed71beca7c428232d48
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-11-19 14:25:12 +0300

    restaurant crud
    `create` and `readAll...` method created and passed tests.

--- src/api/controllers/restaurants.ts
+++ src/api/controllers/restaurants.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-import { restaurantMock } from '../mocks';
 import MessageResponse from '../../interfaces/MessageResponse';
 import { DbService } from '../services';
 import { Restaurant } from '../../interfaces/Restaurant';
 import { ObjectId } from 'mongodb';
-import { Inserted } from '../../interfaces/responses/insertedResponse';
 
 // definitions
 const router = express.Router();
-const dbService = new DbService();
+// create a `restaurants` db session
+const dbService = new DbService('restaurants');
 
 // get all restaurants
 router.get<{}, MessageResponse>('/', async (req, res) => {
@@ -16,7 +15,7 @@ router.get<{}, MessageResponse>('/', async (req, res) => {
     const results: Restaurant[] = await dbService.readAllOrByFilter();
     res.json({
       message: 'restaurants list',
-      response: { restaurantMock, ...results },
+      response: results,
       status: 200,
     });
   } catch (error) {
@@ -27,15 +26,22 @@ router.get<{}, MessageResponse>('/', async (req, res) => {
 // create a restaurant
 router.post<{}, MessageResponse>('/create', async (req, res) => {
   try {
-    const doc: Restaurant = req.body;
-    const result: Inserted = await dbService.create(doc).then(() => {
-      let insertedDoc = dbService.readAllOrByFilter({
-        _id: new ObjectId(result.insertedId),
-      });
+    // get body
+    const doc = req.body;
+    console.log('doc in restaurant create', doc);
+
+    // create object
+    const result = await dbService.create(doc);
+
+    // inserted one
+    const insertedOne = await dbService.readAllOrByFilter({
+      _id: new ObjectId(result.insertedId),
     });
+    console.log('inserted rest', insertedOne);
+
     res.json({
       message: 'object created successfully',
-      response: result,
+      response: insertedOne,
       status: 201,
     });
   } catch (error) {
